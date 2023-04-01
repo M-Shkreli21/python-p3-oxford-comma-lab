ea19c15aa91629cf80d960d7525b2405d10613fd
Mark Shkreli <m.shkreli96@icloud.com>
2023-04-01 16:48:17 -0400

    Lab Done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return ''.join(items)
+    elif len(items) == 2:
+        return ' and '.join(items)
+    else:
+        result = ', '.join(str(i) for i in items[:-1])
+        result += f", and {items[-1]}"
+        return result
